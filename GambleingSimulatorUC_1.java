5e421edeff693bc274e6dcfc39d5b68d035abbe0
ShruthinSatheesh300 <vishnuofch@gmail.com>
2021-12-22 08:45:36 +0530

    Added :Amount of stake and bet amount

---
+++ GambleingSimulatorUC_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+ public class GambleingSimulatorUC_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter amount of stake :");
+		 int amount = sc.nextInt();
+		 System.out.println("Enter amount to bet after every play :");
+		 int bet = sc.nextInt();
+		 System.out.println("Amount of stake is "+amount);
+		 System.out.println("Amount to bet after every game "+bet);
+
+	}
+
+}
